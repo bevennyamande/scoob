bd7b1b85f8b99985183afe2806c7c4700d3bb4b9
Beven Nyamande <bevennyamande@gmail.com>
2020-04-12 14:09:11 +0200

    foreground

--- scoob.py
+++ scoob.py
@@ -1,3 +1,8 @@
 import click
 import requests
 
+
+# get userinput http address
+# get all links on the website and stay inscope
+# for every page search for comment and aggregate them
+# provide a nice output interface
